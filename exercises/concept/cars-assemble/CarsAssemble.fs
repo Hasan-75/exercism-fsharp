6f32737c8614fdae0df051f9e7476d53af8e4ad3
Mahmudul Hasan <m.hasan116114@gmail.com>
2022-01-02 01:31:55 +0600

    Solved Cars Assemble

--- exercises/concept/cars-assemble/CarsAssemble.fs
+++ exercises/concept/cars-assemble/CarsAssemble.fs
@@ -1,10 +1,24 @@
 module CarsAssemble
 
+let prodPerHour : float = 221
+
 let successRate (speed: int): float =
-    failwith "Please implement the 'successRate' function"
+    match speed with
+    | 0 -> 0.0
+    | x when x < 5 -> 1.0
+    | x when x < 9 -> 0.9
+    | 9 -> 0.8
+    | 10 -> 0.77
+    | _ -> failwith("Speed must be in range 0 - 10")
 
 let productionRatePerHour (speed: int): float =
-    failwith "Please implement the 'productionRatePerHour' function"
+    speed
+    |> successRate
+    |> (*) prodPerHour
+    |> (*) (float speed)
 
 let workingItemsPerMinute (speed: int): int =
-    failwith "Please implement the 'workingItemsPerMinute' function"
+    speed
+    |> productionRatePerHour
+    |> fun x -> x / 60.0
+    |> int
