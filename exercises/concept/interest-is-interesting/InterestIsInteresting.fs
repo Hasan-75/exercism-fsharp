f8fef44d8f9e192e7c84c57e1e8a4373b817dad4
Mahmudul Hasan <m.hasan116114@gmail.com>
2022-01-03 00:39:10 +0600

    Solved Interest Is Interesting

--- exercises/concept/interest-is-interesting/InterestIsInteresting.fs
+++ exercises/concept/interest-is-interesting/InterestIsInteresting.fs
@@ -1,13 +1,35 @@
 module InterestIsInteresting
 
+let percentToDecimal (percent: single) : decimal =
+    percent
+    |> decimal
+    |> (*) 0.01m
+
 let interestRate (balance: decimal): single =
-    failwith "Please implement the 'interestRate' function"
+    match balance with
+    | x when x < 0m -> 3.213f
+    | x when x < 1000m -> 0.5f
+    | x when x >= 1000m && x < 5000m -> 1.621f
+    | _ -> 2.475f
 
 let interest (balance: decimal): decimal =
-   failwith "Please implement the 'interest' function"
+   balance
+   |> interestRate
+   |> percentToDecimal
+   |> (*) balance
 
 let annualBalanceUpdate(balance: decimal): decimal =
-   failwith "Please implement the 'annualBalanceUpdate' function"
+   balance
+   |> interest
+   |> (+) balance
 
 let amountToDonate(balance: decimal) (taxFreePercentage: float): int =
-   failwith "Please implement the 'amountToDonate' function"
+    match balance < 0m with
+    | true -> 0
+    | false ->
+       taxFreePercentage
+       |> single
+       |> percentToDecimal
+       |> (*) balance
+       |> (*) 2m
+       |> int
