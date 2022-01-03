4a8756bd080407299e319bacc7a5363bfa2cbf4a
Mahmudul Hasan <m.hasan116114@gmail.com>
2022-01-03 17:57:01 +0600

    Solved Queen Attack

--- exercises/practice/queen-attack/QueenAttack.fs
+++ exercises/practice/queen-attack/QueenAttack.fs
@@ -1,5 +1,16 @@
 ﻿module QueenAttack
 
-let create (position: int * int) = failwith "You need to implement this function."
+let create (position: int * int) : bool =
+    (fst position >= 0 && fst position < 8)
+    &&
+    (snd position >= 0 && snd position < 8)
 
-let canAttack (queen1: int * int) (queen2: int * int) = failwith "You need to implement this function."
+let canAttack (queen1: int * int) (queen2: int * int) =
+    let x1, y1 = queen1
+    let x2, y2 = queen2
+
+    x1 = x2
+    ||
+    y1 = y2
+    ||
+    abs (x1 - x2) = abs (y1 - y2)
