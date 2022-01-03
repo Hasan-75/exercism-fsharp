9583fa536ed5b58efc55035557a3653ccf4742b3
Mahmudul Hasan <m.hasan116114@gmail.com>
2022-01-03 14:56:24 +0600

    Solved Bird Watcher

--- exercises/concept/bird-watcher/BirdWatcher.fs
+++ exercises/concept/bird-watcher/BirdWatcher.fs
@@ -1,19 +1,28 @@
 module BirdWatcher
 
 let lastWeek: int[] =
-   failwith "Please implement the 'lastWeek' value"
+   [| 0; 2; 5; 3; 7; 8; 4 |]
 
 let yesterday(counts: int[]): int =
-  failwith "Please implement the 'yesterday' function"
+  counts.Length - 2
+  |> Array.get counts
 
 let total(counts: int[]): int =
-  failwith "Please implement the 'total' function"
+  counts
+  |> Array.sum
 
 let dayWithoutBirds(counts: int[]): bool =
-  failwith "Please implement the 'dayWithoutBirds' function"
+  counts
+  |> Array.contains 0
 
 let incrementTodaysCount(counts: int[]): int[] =
- failwith "Please implement the 'incrementTodaysCount' function"
+    let lastIndex =counts.Length - 1
+    do counts.[lastIndex] <- counts.[lastIndex] + 1
+    counts
 
 let oddWeek(counts: int[]): bool =
-  failwith "Please implement the 'oddWeek' function"
+    match counts with
+    | [| _; 0; _; 0; _; 0; _ |] -> true
+    | [| _; 10; _; 10; _; 10; _ |] -> true
+    | [| 5; _; 5; _; 5; _; 5 |] -> true
+    | _ -> false
