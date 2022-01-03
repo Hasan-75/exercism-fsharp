400cebb8c4e673b2daa711794354888d31b0756b
Mahmudul Hasan <m.hasan116114@gmail.com>
2022-01-03 17:28:26 +0600

    Solved Two Fer

--- exercises/practice/two-fer/TwoFer.fs
+++ exercises/practice/two-fer/TwoFer.fs
@@ -1,3 +1,10 @@
 ﻿module TwoFer
 
-let twoFer (input: string option): string = failwith "You need to implement this function."
+
+let twoFer (input: string option): string =
+    let statement =
+        sprintf "One for %s, one for me."
+
+    match input with
+    | Some name -> statement name
+    | None -> statement "you"
