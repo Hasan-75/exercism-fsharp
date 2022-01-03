7193dccc21330065944bef436c73785dbb43a459
Mahmudul Hasan <m.hasan116114@gmail.com>
2022-01-03 16:51:41 +0600

    Solved Pizza Pricing

--- exercises/concept/pizza-pricing/PizzaPricing.fs
+++ exercises/concept/pizza-pricing/PizzaPricing.fs
@@ -1,7 +1,28 @@
 module PizzaPricing
 
-// TODO: please define the 'Pizza' discriminated union type
+type Pizza =
+    | Margherita
+    | Caprese
+    | Formaggio
+    | ExtraSauce of Pizza
+    | ExtraToppings of Pizza
 
-let pizzaPrice (pizza: Pizza): int = failwith "Please implement the 'pizzaPrice' function"
+let rec pizzaPrice (pizza: Pizza): int =
+    match pizza with
+    | Margherita -> 7
+    | Caprese -> 9
+    | Formaggio -> 10
+    | ExtraSauce p -> 1 + (pizzaPrice p)
+    | ExtraToppings p -> 2 + (pizzaPrice p)
 
-let orderPrice(pizzas: Pizza list): int = failwith "Please implement the 'orderPrice' function"
+let calculateAdditionalFee (pizzas: Pizza list) : int =
+    match pizzas.Length with
+    | 1 -> 3
+    | 2 -> 2
+    | _ -> 0
+
+let orderPrice(pizzas: Pizza list): int =
+    pizzas
+    |> List.map pizzaPrice
+    |> List.sum
+    |> (+) (calculateAdditionalFee pizzas)
