bbc4bb4c415f5418ffbf6c3d464f0705c5cec432
Mahmudul Hasan <m.hasan116114@gmail.com>
2022-01-03 17:20:15 +0600

    Solved Tisbury Treasure Hunt

--- exercises/concept/tisbury-treasure-hunt/TisburyTreasureHunt.fs
+++ exercises/concept/tisbury-treasure-hunt/TisburyTreasureHunt.fs
@@ -1,13 +1,31 @@
 module TisburyTreasureHunt
 
+let charToNumeric (ch: char) : int =
+    int ch - int '0'
+
 let getCoordinate (line: string * string): string =
-    failwith "Please implement the 'getCoordinate' function"
+    snd line
 
 let convertCoordinate (coordinate: string): int * char = 
-    failwith "Please implement the 'convertCoordinate' function"
+    let chars = coordinate.ToCharArray()
+    (charToNumeric chars.[0], chars.[1])
 
 let compareRecords (azarasData: string * string) (ruisData: string * (int * char) * string) : bool = 
-    failwith "Please implement the 'compareRecords' function"
+    let (_, rCoord, _) = ruisData
+    let aCoord = 
+        azarasData 
+        |> getCoordinate
+        |> convertCoordinate
+    aCoord = rCoord
 
 let createRecord (azarasData: string * string) (ruisData: string * (int * char) * string) : (string * string * string * string) =
-    failwith "Please implement the 'createRecord' function"
+    let (treasureName, coordinate) = azarasData
+    let (location, _, quadrant) = ruisData
+
+    let isSame = compareRecords azarasData ruisData
+
+    match isSame with
+    | true ->
+        ( coordinate, location, quadrant, treasureName )
+    | false ->
+        ( "", "", "", "" )
