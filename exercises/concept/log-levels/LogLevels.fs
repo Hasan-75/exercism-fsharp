2af0a4e697edd0acf3b83b4759a41e7efce42e7c
Mahmudul Hasan <m.hasan116114@gmail.com>
2022-01-03 13:01:01 +0600

    Solved Log Levels

--- exercises/concept/log-levels/LogLevels.fs
+++ exercises/concept/log-levels/LogLevels.fs
@@ -1,7 +1,65 @@
 module LogLevels
 
-let message (logLine: string): string = failwith "Please implement the 'message' function"
+type LogLevel =
+    | INFO of string
+    | WARNING of string
+    | ERROR of string
 
-let logLevel(logLine: string): string = failwith "Please implement the 'logLevel' function"
+let clean (s: string) : string =
+    s.Trim()
 
-let reformat(logLine: string): string = failwith "Please implement the 'reformat' function"
+let parseLog (logLine: string) : LogLevel =
+    let words =
+        logLine
+        |> fun s -> s.Split([|' '|])
+        |> List.ofArray
+        |> List.map clean
+
+    let wordsToStr (wSeq: seq<string>) =
+        String.concat " " wSeq
+        |> clean
+
+    match words with
+    | "[INFO]:" :: rest ->
+        rest
+        |> wordsToStr
+        |> INFO
+    | "[ERROR]:" :: rest ->
+        rest
+        |> wordsToStr
+        |> ERROR
+    | "[WARNING]:" :: rest ->
+        rest
+        |> wordsToStr
+        |> WARNING
+    | _ -> failwith "Invalid level"
+
+let message (logLine: string): string =
+    let log = parseLog logLine
+
+    match log with
+    | INFO msg
+    | WARNING msg
+    | ERROR msg
+        -> msg
+
+
+let logLevel(logLine: string): string =
+    let log = parseLog logLine
+
+    match log with
+    | INFO _ -> "info"
+    | ERROR _ -> "error"
+    | WARNING _ -> "warning"
+
+
+let reformat(logLine: string): string =
+    let level =
+        logLine
+        |> logLevel
+
+    let msg =
+        logLine
+        |> message
+
+    sprintf "%s (%s)" msg level
