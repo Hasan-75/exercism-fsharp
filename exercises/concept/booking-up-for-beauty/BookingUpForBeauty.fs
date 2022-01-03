db4ec55e35efc2a3879d876275a1d0dddc583a4c
Mahmudul Hasan <m.hasan116114@gmail.com>
2022-01-03 13:56:28 +0600

    Solved Booking Up For Beauty

--- exercises/concept/booking-up-for-beauty/BookingUpForBeauty.fs
+++ exercises/concept/booking-up-for-beauty/BookingUpForBeauty.fs
@@ -3,13 +3,22 @@ module BookingUpForBeauty
 // The following line is needed to use the DateTime type
 open System
 
-let schedule (appointmentDateDescription: string): DateTime = failwith "Please implement the 'schedule' function"
+let schedule (appointmentDateDescription: string): DateTime =
+    DateTime.Parse(appointmentDateDescription)
 
-let hasPassed (appointmentDate: DateTime): bool = failwith "Please implement the 'hasPassed' function"
+let hasPassed (appointmentDate: DateTime): bool =
+    let comparison = DateTime.Compare(appointmentDate, DateTime.Now);
+    comparison < 0
 
 let isAfternoonAppointment (appointmentDate: DateTime): bool =
-    failwith "Please implement the 'isAfternoonAppointment' function"
+    appointmentDate.Hour >= 12
+    &&
+    appointmentDate.Hour < 18
 
-let description (appointmentDate: DateTime): string = failwith "Please implement the 'description' function"
+let description (appointmentDate: DateTime): string =
+    appointmentDate.ToString()
+    |> sprintf "You have an appointment on %s."
 
-let anniversaryDate(): DateTime = failwith "Please implement the 'anniversaryDate' function"
+let anniversaryDate(): DateTime =
+    DateTime(2019, 9, 15, 0, 0, 0)
+        .AddYears(DateTime.Now.Year - 2019)
