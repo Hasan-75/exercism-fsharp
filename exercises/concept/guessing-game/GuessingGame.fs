b8df236c3d1060677516b4b288b81b9468b08532
Mahmudul Hasan <m.hasan116114@gmail.com>
2022-01-03 14:05:19 +0600

    Solved Guessing Game

--- exercises/concept/guessing-game/GuessingGame.fs
+++ exercises/concept/guessing-game/GuessingGame.fs
@@ -1,3 +1,11 @@
 module GuessingGame
 
-let reply (guess: int): string = failwith "Please implement the 'reply' function"
+let selected = 42
+
+let reply (guess: int): string =
+    match guess - selected with
+    | 0 -> "Correct"
+    | -1
+    | 1 -> "So close"
+    | x when x < -1 -> "Too low"
+    | _ -> "Too high"
