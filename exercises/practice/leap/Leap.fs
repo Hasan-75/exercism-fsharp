3e1114842d7095735dfcd9b8d6c6956cb3ed45f5
Mahmudul Hasan <m.hasan116114@gmail.com>
2022-01-03 17:41:31 +0600

    Solved Leap

--- exercises/practice/leap/Leap.fs
+++ exercises/practice/leap/Leap.fs
@@ -1,3 +1,24 @@
 ﻿module Leap
 
-let leapYear (year: int): bool = failwith "You need to implement this function."
+let divisibleBy x y =
+    y % x = 0
+
+let divisibleBy4 =
+    divisibleBy 4
+
+let divisibleBy100 =
+    divisibleBy 100
+
+let divisibleBy400 =
+    divisibleBy 400
+
+let leapYear (year: int): bool =
+    match divisibleBy4 year with
+    | true ->
+        match divisibleBy100 year with
+        | true ->
+            match divisibleBy400 year with
+            | true -> true
+            | false -> false
+        | false -> true
+    | false -> false
