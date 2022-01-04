beab7032be598d7c28c3ce89b4c4746b1759bdac
Mahmudul Hasan <m.hasan116114@gmail.com>
2022-01-04 11:44:29 +0600

    Solved Raindrops

--- exercises/practice/raindrops/Raindrops.fs
+++ exercises/practice/raindrops/Raindrops.fs
@@ -1,3 +1,24 @@
 ﻿module Raindrops
 
-let convert (number: int): string = failwith "You need to implement this function."
+let getWord (x: int) : string =
+    match x with
+    | 3 -> "Pling"
+    | 5 -> "Plang"
+    | 7 -> "Plong"
+    | _ -> ""
+
+let getSound (y: int) (x: int): string =
+    match y % x = 0 with
+    | true -> getWord x
+    | false -> ""
+
+let convert (number: int): string =
+    let sound =
+        [3; 5; 7]
+        |> List.map (getSound number)
+        |> List.fold
+            (fun result slice-> result + slice) ""
+
+    match sound with
+    | "" -> number |> string
+    | _ -> sound
