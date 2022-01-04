9d2d6de05b4d15346da85670f44ae1da113d6fdd
Mahmudul Hasan <m.hasan116114@gmail.com>
2022-01-04 13:34:34 +0600

    Solved Space Age

--- exercises/practice/space-age/SpaceAge.fs
+++ exercises/practice/space-age/SpaceAge.fs
@@ -1,5 +1,34 @@
 ﻿module SpaceAge
 
-// TODO: define the Planet type
+type Planet =
+| Mercury
+| Venus
+| Earth
+| Mars
+| Jupiter
+| Saturn
+| Uranus
+| Neptune
 
-let age (planet: Planet) (seconds: int64): float = failwith "You need to implement this function."
+let oneEarthYearInSeconds = 31557600L
+
+let secondsToEarthYear (seconds: int64) : float =
+    oneEarthYearInSeconds
+    |> float
+    |> (/) (float seconds)
+
+let relativeOrbitalPeriodInYears (planet: Planet) : float =
+    match planet with
+    | Mercury -> 0.2408467
+    | Venus -> 0.61519726
+    | Earth -> 1
+    | Mars -> 1.8808158
+    | Jupiter -> 11.862615
+    | Saturn -> 29.447498
+    | Uranus -> 84.016846
+    | Neptune -> 164.79132
+
+let age (planet: Planet) (seconds: int64): float =
+    planet
+    |> relativeOrbitalPeriodInYears
+    |> (/) (secondsToEarthYear seconds)
